b9f6cceea35086d609484eee58f78dec28c30a66
tduclos <67517520+tduclos@users.noreply.github.com>
2020-09-01 09:19:24 -0400

    routes

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const Sequelize = require('sequelize');
 const User = require('../models').User;
 const Course = require('../models').Course;
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+const { check, validationResult } = require('express-validator');
+let coursesArray = [];
 
 //asyncHandler
 function asyncHandler(callback){
@@ -16,6 +20,43 @@ function asyncHandler(callback){
       }
     }
   }
+
+//authenticateUser
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+  // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+  if (credentials) {
+    //retrieve user
+        const users = await User.findAll();
+        const user = users.find(user => user.emailAddress === credentials.name);
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for : ${user.firstName} ${user.lastName}`);
+        //store user on request header
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.firstName} ${user.lastName}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+  //authentication failure
+  if (message) {
+    console.warn(message);
+  
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    //authentication success
+      next();
+  }
+};
 
 //GET all courses
 router.get('/api/courses', asyncHandler(async (req, res, next) => {
@@ -36,24 +77,64 @@ router.get('/api/courses', asyncHandler(async (req, res, next) => {
 }));
 
 //GET specific course
-router.get('/api/courses/:id', asyncHandler(async (req, res, next) => {
-
-}));
+router.get('/courses/:id', asyncHandler(async (req, res, next) => {
+    const course = await Course.findByPk(req.params.id, {
+      attributes: { exclude: ['createdAt', 'updatedAt'] }
+    });
+    console.log(course);
+    res.json({ course });
+  }));
   
 //POST New Course 
-router.post('/api/courses', [], asyncHandler(async (req, res, next) => {
+router.post('/api/courses', [
+  check('title')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please include a title with your POST request'),
+check('description')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please include a description with your POST request')
+], asyncHandler(async (req, res, next) => {
   
-}));
+  const errors = validationResult(req);
 
+  //validation error handling
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(error => error.msg);
+    return res.status(401).json({ errors: errorMessages });
+    const course = await Course.create(req.body);
+    coursesArray.push(course);
+    console.log(course);
+    return res.status(201).end();
+  }));
 
 //PUT course updates
-router.put('/api/courses/:id', asyncHandler(async (req, res, next) => {
-  
+router.put('/api/courses/:id', authenticateUser, asyncHandler(async (req, res, next) => {
+  console.log('Starting');
+  let user = req.currentUser;
+  let course = await Course.findByPk(req.params.id);
+  console.log(course.userId);
+  if (user.id === course.userId) {
+    if (req.body.title === undefined || req.body.description === undefined) {  
+      res.status(400).send({ error: 'Please provide a valid for the title and the description'});
+    } else {
+      await course.update(req.body);
+      res.sendStatus(204);
+    }
+  } else {
+    res.status(403).send({ error: 'This user does not own this course'});
+  }
 }));
 
 //DELETE a course
-router.delete('/api/courses/:id', asyncHandler(async (req ,res) => {
-
-}));
+router.delete('/api/courses/:id', authenticateUser, asyncHandler(async (req ,res) => {
+    let user = req.currentUser;
+    let course = await Course.findByPk(req.params.id);
+      if (user.id === course.userId) {
+          course.destroy();
+          res.sendStatus(204);
+        } else {
+          res.status(403).send({ error: 'You does not have access to this course'});
+        }
+  }));
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const Sequelize = require('sequelize');
 const User = require('../models').User;
 const Course = require('../models').Course;
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+const { check, validationResult } = require('express-validator');
+const users = [];
 
 //asyncHandler
 function asyncHandler(callback){
@@ -17,15 +21,96 @@ function asyncHandler(callback){
     } 
   }
 
-//GET User
-router.get('/api/users/', asyncHandler(async (req, res, next) => {
+//authenticateUser
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+  // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+  if (credentials) {
+    //retrieve user
+        const users = await User.findAll();
+        const user = users.find(user => user.emailAddress === credentials.name);
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for : ${user.firstName} ${user.lastName}`);
+        //store user on request header
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.firstName} ${user.lastName}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+  //authentication failure
+  if (message) {
+    console.warn(message);
+  
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    //authentication success
+      next();
+  }
+};
 
+//GET User
+router.get('/api/users', authenticateUser, asyncHandler(async (req, res, next) => {
+  console.log('Starting');
+    let authedUser = req.currentUser;
+    if(authedUser) {
+      res.json({
+        id: authedUser.id,
+        firstName: authedUser.firstName,
+        lastName: authedUser.lastName,
+        emailAddress: authedUser.emailAddress
+      });
+    } else {
+      res.sendStatus(404);
+    }
 }));
   
 
 //POST New user
-router.post('/api/users/', [], asyncHandler(async (req, res, next) => {
- 
+router.post('/api/users', [
+    check('firstName')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please include a first name with your POST request'),
+  check('lastName')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please include a last name with your POST request'),
+  check('emailAddress')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please include an email addresswith your POST request'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please include a password with your POST request'),
+], asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+  
+    //validation error handling
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map(error => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+  
+    let user = req.body;
+    user.password = bcryptjs.hashSync(user.password);
+    await User.create(user);const currentUsers = await User.findAll({
+      attributes: ['emailAddress']
+    });
+    let foundUserEmails = JSON.stringify(currentUsers);
+    if (user.emailAddress === foundUserEmails) {
+      console.log('There is already a user associated with this e-mail address')
+      return res.status(500).end();
+    } else {
+      users.push(user);
+      res.location('/');
+      return res.status(201).end();
+    }  
 }));
-
 module.exports = router;
